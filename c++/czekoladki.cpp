9ee855bc377fac06063a91ac84bb2ba66cf1e620
Mateusz Jasiński <69435721+wizarddos@users.noreply.github.com>
2024-09-16 12:33:32 +0200

    Dodane 2 kolejne zadania

---
+++ c++/czekoladki.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int w1,w2,k1,k2;
+	
+	cin >> w1 >> k1 >> w2 >> k2;
+	cout << (w1*k1) + (w2*k2);
+}
---
+++ c++/gwiazdki.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	cout << "  *" << "\n";
+	cout << " **" << "\n";
+	cout << "***" << "\n";
+	
+	return 0;
+}
