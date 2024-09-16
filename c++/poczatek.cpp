e8c669ccc19493b02dd14f036fab33723eb14960
Mateusz Jasiński <69435721+wizarddos@users.noreply.github.com>
2024-09-16 13:21:33 +0200

    Idc

---
+++ c++/poczatek.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Mount Everest - 8850 m n.p.m. \n";
+	cout << "McKinley - 6194 m n.p.m. \n";
+	cout << "Mont Blanc - 4810 m n.p.m.";
+}
